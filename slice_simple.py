d98548fe6e8efccc9a077dc50dbb45f6e8d2075b
agustinportoo <tanitoporto@gmail.com>
2026-03-20 15:07:00 -0300

    Update slice_simple.py

--- slice_simple.py
+++ slice_simple.py
@@ -3,3 +3,7 @@ def slice_simple():
     usando slicing y lower().
     """
     texto = "Awesome"
+    texto_m= (texto.lower())
+    print(texto_m[0:3])
+    print(texto_m[2:5])
+    print(texto_m) 
