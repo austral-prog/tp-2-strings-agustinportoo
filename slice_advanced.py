f7f967248642ee3560c11b3f2a02fb9aca1997f1
agustinportoo <tanitoporto@gmail.com>
2026-03-24 16:36:57 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -2,6 +2,5 @@ def slice_advanced():
     """Lee un texto e imprime los caracteres desde la posición 4
     en adelante, tomando uno de cada dos (paso 2).
     """
-    pass
     texto=input("Texto: ")
     print(texto[4::2])
